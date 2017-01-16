d6538b134f002fdf98d611d895e035b7a5c4f97f
Amal Thomas <amalgta@gmail.com>
2017-01-16 17:48:37 +0530

    GreenList project initial commit.

---
+++ app/src/main/java/com/styx/mobile/greenlist/ListingDetailActivity.java
@@ -0,0 +1,42 @@
+package com.styx.mobile.greenlist;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import eu.fiskur.simpleviewpager.ImageURLLoader;
+import eu.fiskur.simpleviewpager.SimpleViewPager;
+
+public class ListingDetailActivity extends AppCompatActivity {
+
+    SimpleViewPager simpleViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        SimpleViewPager simpleViewPager = (SimpleViewPager) findViewById(R.id.iv_hero_image);
+
+        String[] demoUrlArray = new String[]{
+                "http://fiskur.eu/apps/simpleviewpagerdemo/001.jpg",
+                "http://fiskur.eu/apps/simpleviewpagerdemo/002.jpg",
+                "http://fiskur.eu/apps/simpleviewpagerdemo/003.jpg",
+                "http://fiskur.eu/apps/simpleviewpagerdemo/004.jpg",
+                "http://fiskur.eu/apps/simpleviewpagerdemo/005.jpg",
+        };
+        simpleViewPager.setImageUrls(demoUrlArray, new ImageURLLoader() {
+            @Override
+            public void loadImage(ImageView view, String url) {
+                Picasso.with(ListingDetailActivity.this).load(url).into(view);
+            }
+        });
+        int indicatorColor = Color.parseColor("#ffffff");
+        int selectedIndicatorColor = Color.parseColor("#fff000");
+        simpleViewPager.showIndicator(indicatorColor, selectedIndicatorColor);
+    }
+}
--- app/src/main/java/com/styx/mobile/greenlist/SearchActivity.java
+++ app/src/main/java/com/styx/mobile/greenlist/SearchActivity.java
@@ -1,15 +1,66 @@
 package com.styx.mobile.greenlist;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.styx.mobile.greenlist.utils.Utils;
 
 public class SearchActivity extends AppCompatActivity {
+    boolean isFilterVisible = false;
+    FloatingActionButton floatingActionButtonFilter;
+    View filters_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //initializeToolbar();
+        setContentView(R.layout.activity_search);
+        filters_layout = findViewById(R.id.filters_layout);
+        floatingActionButtonFilter = (FloatingActionButton) findViewById(R.id.floatingActionButtonFilter);
+        floatingActionButtonFilter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabRevealAnimation();
+            }
+        });
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isFilterVisible) {
+            fabRevealAnimation();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    private void fabRevealAnimation() {
+        if (isFilterVisible) {
+            filters_layout.setVisibility(View.INVISIBLE);
+            isFilterVisible = false;
+        } else {
+            filters_layout.setVisibility(View.VISIBLE);
+            isFilterVisible = true;
+        }
+    }
 }
