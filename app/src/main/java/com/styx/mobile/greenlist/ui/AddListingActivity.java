e026f910fc285d792f975c01020b4eb3030d0a7f
Amal Thomas <amalgta@gmail.com>
2017-01-17 18:15:23 +0530

    Add Listing form UI completed.

--- app/src/main/java/com/styx/mobile/greenlist/ui/AddListingActivity.java
+++ app/src/main/java/com/styx/mobile/greenlist/ui/AddListingActivity.java
@@ -1,60 +1,51 @@
 package com.styx.mobile.greenlist.ui;
 
-import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 
 import com.styx.mobile.greenlist.R;
+import com.styx.mobile.greenlist.models.Listing;
 import com.styx.mobile.greenlist.models.Type;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
-import io.realm.OrderedRealmCollection;
 import io.realm.Realm;
-import io.realm.RealmBaseAdapter;
-import io.realm.RealmConfiguration;
-import io.realm.RealmModel;
-import io.realm.RealmQuery;
 import io.realm.RealmResults;
-import io.realm.Sort;
 
 public class AddListingActivity extends AppCompatActivity {
     Realm realm;
-
+    Listing newListing;
+    Spinner spinnerType;
+    EditText editTextListingName, editTextMinPrice, editTextMaxPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         realm = Realm.getDefaultInstance();
-
         setContentView(R.layout.activity_addlisting);
-        initalizeForm();
+        initializeUI();
     }
 
-    void initalizeForm() {
-        Spinner spinnerType = (Spinner) findViewById(R.id.spinnerType);
-        final RealmResults<Type> typeRealmResults = realm.where(Type.class).findAll();
+    void initializeUI() {
+        newListing = new Listing();
+
+        spinnerType = (Spinner) findViewById(R.id.spinnerType);
+        editTextListingName = (EditText) findViewById(R.id.editTextListingName);
+        editTextMinPrice = (EditText) findViewById(R.id.editTextMinPrice);
+        editTextMaxPrice = (EditText) findViewById(R.id.editTextMaxPrice);
 
         ArrayList<String> arrayList = new ArrayList<>();
-
-        RealmResults<Type> types = realm.where(Type.class).findAll();
-        for (Type type : types) {
+        RealmResults<Type> typeRealmResults = realm.where(Type.class).findAll();
+        for (Type type : typeRealmResults) {
             arrayList.add(type.getName());
         }
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+
         spinnerType.setAdapter(adapter);
+
     }
 
     @Override
