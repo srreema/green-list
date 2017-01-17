9b7eded98ada5f486ac3ce08184c1eb9a83977e1
Amal Thomas <amalgta@gmail.com>
2017-01-17 19:18:40 +0530

    Add Listing form UI completed.

--- app/src/main/java/com/styx/mobile/greenlist/ui/AddListingActivity.java
+++ app/src/main/java/com/styx/mobile/greenlist/ui/AddListingActivity.java
@@ -1,13 +1,25 @@
 package com.styx.mobile.greenlist.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.vision.text.Text;
 import com.styx.mobile.greenlist.R;
 import com.styx.mobile.greenlist.models.Listing;
+import com.styx.mobile.greenlist.models.Location;
 import com.styx.mobile.greenlist.models.Type;
 
 import java.util.ArrayList;
@@ -19,7 +31,11 @@ public class AddListingActivity extends AppCompatActivity {
     Realm realm;
     Listing newListing;
     Spinner spinnerType;
-    EditText editTextListingName, editTextMinPrice, editTextMaxPrice;
+    TextView textViewLocationName;
+    LinearLayout linearLayoutLocation;
+    EditText editTextName, editTextMinPrice, editTextMaxPrice;
+    int REQUEST_CODE_PLACE_PICKER = 1000;
+    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +47,24 @@ public class AddListingActivity extends AppCompatActivity {
 
     void initializeUI() {
         newListing = new Listing();
-
         spinnerType = (Spinner) findViewById(R.id.spinnerType);
-        editTextListingName = (EditText) findViewById(R.id.editTextListingName);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        textViewLocationName = (TextView) findViewById(R.id.textViewLocationName);
         editTextMinPrice = (EditText) findViewById(R.id.editTextMinPrice);
+        linearLayoutLocation = (LinearLayout) findViewById(R.id.linearLayoutLocation);
         editTextMaxPrice = (EditText) findViewById(R.id.editTextMaxPrice);
+        linearLayoutLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = builder.build(AddListingActivity.this);
+                    startActivityForResult(intent, REQUEST_CODE_PLACE_PICKER);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         ArrayList<String> arrayList = new ArrayList<>();
         RealmResults<Type> typeRealmResults = realm.where(Type.class).findAll();
@@ -43,9 +72,21 @@ public class AddListingActivity extends AppCompatActivity {
             arrayList.add(type.getName());
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
-
         spinnerType.setAdapter(adapter);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_PLACE_PICKER) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                Location location = new Location();
+                location.setName(place.getName().toString());
+                location.setLatitude(String.valueOf((place.getLatLng()).latitude));
+                location.setLongitude(String.valueOf((place.getLatLng()).longitude));
+                newListing.setLocation(location);
+                textViewLocationName.setText(String.format(getString(R.string.location_name), place.getName()));
+            }
+        }
     }
 
     @Override
