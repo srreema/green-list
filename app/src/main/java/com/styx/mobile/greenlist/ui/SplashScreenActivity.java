5c24ce57765864358771ff0e1bffaf1689599294
Amal Thomas <amalgta@gmail.com>
2017-02-02 10:49:36 +0530

    Splash Screen

---
+++ app/src/main/java/com/styx/mobile/greenlist/ui/SplashScreenActivity.java
@@ -0,0 +1,19 @@
+package com.styx.mobile.greenlist.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ *
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
